c3031baeb4de6bc39d9dc27ba7693195929448d0
ToddWebDev <webdevtodd@gmail.com>
2018-09-01 13:56:49 -0500

    Add Camera with flip and shutter

--- src/components/ColorMatch.js
+++ src/components/ColorMatch.js
@@ -1,19 +1,67 @@
 import React, { Component } from 'react'
-import { Text, View, Button } from 'react-native'
+import { Text, View, TouchableOpacity, TouchableHighlight } from 'react-native'
 import { createBottomTabNavigator } from 'react-navigation';
 import { Icon } from 'react-native-elements';
+import { Permissions, Camera } from 'expo';
 import CameraRoll from '../components/CameraRoll';
 
 class ColorMatch extends Component {
+  state = {
+    hasCameraPermission: null,
+    type: Camera.Constants.Type.back,
+  };
+
+  async componentWillMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === 'granted' });
+  }
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.text}> Init Color Match Flow </Text>
-        <Button title='Finish' onPress={
-          () => this.props.navigation.navigate('ColorMatchResults')
-        }></Button> 
-      </View>
-    )
+    const { hasCameraPermission } = this.state;
+
+    if (hasCameraPermission === null) {
+      return <View />;
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+        <View style={{ flex: 1 }}>
+          <Camera style={{ flex: 1 }} type={this.state.type} ref={ref => { this.camera = ref; }}>
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: 'transparent',
+                flexDirection: 'row',
+              }}>
+              <TouchableOpacity
+                style={styles.flipBtn}
+                onPress={() => {
+                  this.setState({
+                    type: this.state.type === Camera.Constants.Type.back
+                      ? Camera.Constants.Type.front
+                      : Camera.Constants.Type.back,
+                  });
+                }}>
+                <Text><Icon name={'switch-camera'} size={30} color={'rgba(255,255,255,0.6)'} /></Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.shutterBtn}
+                onPress={this.takePicture}>
+              </TouchableOpacity>
+            </View>
+          </Camera>
+        </View>
+      );
+    }
+  }
+
+  takePicture = async () => {
+    if (this.camera) {
+       await this.camera.takePictureAsync().then(
+        data => { 
+          console.log(data);
+        })
+    }
   }
 }
 
@@ -55,7 +103,6 @@ export default Tabs;
 const styles = {
   container: {
     flex: 1,
-    backgroundColor: '#333',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -66,5 +113,22 @@ const styles = {
     fontWeight: 'bold',
     textAlign: 'center',
     margin: 13
+  },
+
+  shutterBtn: {
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+    backgroundColor: '#FA5F5F',
+    borderWidth: 2,
+    borderColor: 'white',
+    borderRadius: 50,
+    height: 50,
+    width: 50,
+  },
+
+  flipBtn: {
+    position: 'absolute',
+    top: 10,
+    right: 10
   }
 };
